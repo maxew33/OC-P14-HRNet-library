dd0fc0681599326013e172a983ed7a0955bead5f
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-08-10 18:14:13 +0200

    :building_construction: [src] first component added

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,22 @@
+import React, { FormEvent } from 'react'
+
+interface ModalProps {
+    message?: string
+    label?: string
+    confirm: (e: FormEvent) => void
+}
+
+const Modal: React.FC<ModalProps> = (props) => {
+    return (
+        <div className="confirm-modal">
+            <div className="confirm-modal-wrapper">
+                {props.message}
+                <button className="create-btn" onClick={props.confirm}>
+                    {props.label ?? 'OK'}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
